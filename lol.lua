b47ace334f99fae0c1c80de16739ecbe4b2fefd0
P996Lazer69 <138830027+P996Lazer69@users.noreply.github.com>
2023-07-26 10:49:44 +0100

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,7 +1,7 @@
-    wait(1.7)
+   wait(1.2)
     local players = game:GetService("Players")
     local localplayer = players.LocalPlayer
-    
+
     for _, coreasset in ipairs(game.CoreGui:GetDescendants()) do
         coreasset:Destroy()
     end
@@ -33,8 +33,5 @@
     ImageLabel.Position = UDim2.new(0.444213718, 0, 0.444734752, 0)
     ImageLabel.Size = UDim2.new(0.110689439, 0, 0.198863626, 0)
     ImageLabel.Image = "rbxassetid://14195957999"
-    local queueonteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
-    local teleportScript = [[
-        localplayer:Kick("lol")
-    ]]
-    queueonteleport(teleportScript)
+    task.wait(3)
+    localplayer:Kick("lol")
