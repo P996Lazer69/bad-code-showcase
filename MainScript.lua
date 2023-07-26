948050b53aec44ccad72042e19c7c74531593d75
P996Lazer69 <138830027+P996Lazer69@users.noreply.github.com>
2023-07-26 10:19:25 +0100

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,5 @@
 local queueonteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
 local teleportScript = [[
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/P996Lazer69/memz/main/CrackHub.lua'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/P996Lazer69/memz/main/lol.lua'))()
 ]]
 queueonteleport(teleportScript)
