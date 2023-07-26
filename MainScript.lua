532b3ca0f56286655e89cf9705c6c751a8baff5a
P996Lazer69 <138830027+P996Lazer69@users.noreply.github.com>
2023-07-26 10:16:12 +0100

    Update and rename MainScript to MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,4 @@
+local queueonteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
+local teleportScript = [[
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/P996Lazer69/memz/main/CrackHub.lua'))()
+]]
